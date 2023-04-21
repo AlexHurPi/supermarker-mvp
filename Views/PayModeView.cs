b63fc2f99bcf3f8c007c9f53efb860f359624c2f
Alexander Hurtado Pineda <alexander.hurtado7170@gmail.com>
2023-04-21 11:17:05 -0500

    Pay Mode Presenter

--- Views/PayModeView.cs
+++ Views/PayModeView.cs
@@ -10,11 +10,75 @@ using System.Windows.Forms;
 
 namespace Supermarket_mvp.Views
 {
-    public partial class PayModeView : Form
+    public partial class PayModeView : Form, IpayModeView
     {
+        private bool isEdit;
+        private bool isSuccessful;
+        private string message;
         public PayModeView()
         {
             InitializeComponent();
+            AssociateAndRaiseViewEvents();
+        }
+
+        private void AssociateAndRaiseViewEvents()
+        {
+            BtnSearch.Click += delegate { SearchEvent?.Invoke(this, EventArgs.Empty); };
+            TxtSearch.KeyDown += (s, e) =>
+          {
+              if (e.KeyCode == Keys.Enter)
+              {
+                  SearchEvent?.Invoke(this, EventArgs.Empty);
+              }
+          };
+        }
+
+        public string PayModeId 
+        {
+            get { return TxtPayModeId.Text; }
+            set { TxtPayModeId.Text = value; }
+        }
+        public string PayModeName
+        {
+            get { return TxtPayModeName.Text; }
+            set { TxtPayModeName.Text = value; }
+        }
+        public string PayModeObservation 
+        {
+            get { return TxtPayModeObservation.Text; }
+            set { TxtPayModeObservation.Text = value; }
+        }
+        public string SearchValue
+        {
+            get { return TxtSearch.Text; }
+            set { TxtSearch.Text = value; }
+        }
+        public bool IsEdit
+        {
+            get { return IsEdit; }
+            set { IsEdit = value; }
+        }
+        public bool IsSuccessful
+        {
+            get { return IsSuccessful; }
+            set { IsSuccessful = value; }
+        }
+        public string Message
+        {
+            get { return Message; }
+            set { Message = value; }
+        }
+
+        public event EventHandler SearchEvent;
+        public event EventHandler AddNewEvent;
+        public event EventHandler EditEvent;
+        public event EventHandler DeleteEvent;
+        public event EventHandler SaveEvent;
+        public event EventHandler CancelEvent;
+
+        public void SetPayModeListBildingSource(BindingSource payModeList)
+        {
+            DgPayMode.DataSource= payModeList;
         }
     }
 }
